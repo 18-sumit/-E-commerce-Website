f084af66e82dbfee6b6c1c24aaab85568a6dd9b3
Sumit Singh <sumitsingh3076@gmail.com>
2024-12-06 00:04:06 +0530

    initial minor updates and setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,19 @@
-import { Route, Routes } from "react-router-dom"
-import Home from "./Pages/Home"
-import About from "./Pages/About"
-import Collection from "./Pages/Collection"
-import Contact from "./Pages/Contact"
-import Product from "./Pages/Product"
-import PlaceOrder from "./Pages/PlaceOrder"
-import Orders from "./Pages/Orders"
-import Login from "./Pages/Login"
-import Cart from "./Pages/Cart"
-import Navbar from "./components/Navbar"
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Collection from "./Pages/Collection";
+import Contact from "./Pages/Contact";
+import Product from "./Pages/Product";
+import PlaceOrder from "./Pages/PlaceOrder";
+import Orders from "./Pages/Orders";
+import Login from "./Pages/Login";
+import Cart from "./Pages/Cart";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className="px-4 sm:px[5vw] md:px-[7vw] lg:px-[9vw]">
-
-      <Navbar/>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -27,8 +26,7 @@ const App = () => {
         <Route path="/orders" element={<Orders />} />
       </Routes>
     </div>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
