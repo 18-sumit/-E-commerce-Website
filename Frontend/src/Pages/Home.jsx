70f56d9ae43495821d63b53808765a5576808ce5
Sumit Singh <sumitsingh3076@gmail.com>
2024-12-06 21:36:35 +0530

    home page

---
+++ Frontend/src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import Hero from "../components/Hero";
+import LatestCollection from "../components/LatestCollection";
+
+function Home() {
+  return (
+    <div>
+      <Hero />
+      <LatestCollection />
+    </div>
+  );
+}
+
+export default Home;
