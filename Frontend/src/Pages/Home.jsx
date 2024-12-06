79cec928ba19e4257d33db572c86926560069fb8
Sumit Singh <sumitsingh3076@gmail.com>
2024-12-07 00:31:25 +0530

    all components added for display

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,11 +1,18 @@
+import OurPolicy from "../components/OurPolicy";
+import BestSeller from "../components/BestSeller";
 import Hero from "../components/Hero";
 import LatestCollection from "../components/LatestCollection";
+import NewsLetterBox from "../components/NewsLetterBox";
 
 function Home() {
   return (
     <div>
       <Hero />
       <LatestCollection />
+      <BestSeller />
+      <OurPolicy />
+      <NewsLetterBox />
+
     </div>
   );
 }
