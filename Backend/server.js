8374225641da32d8012db62028f223068fd56cdc
Sumit Singh <sumitsingh3076@gmail.com>
2024-12-11 01:29:59 +0530

    basic app

---
+++ Backend/server.js
@@ -0,0 +1,30 @@
+import express from "express"
+import cors from "cors"
+import 'dotenv/config'
+import connectDB from "./config/mongodb.js";
+import uploadOnCloudinary from "./config/cloudinary.js";
+dotenv.config();
+
+//App config
+
+const app = express();
+
+const port = process.env.port || 4000;
+connectDB();
+uploadOnCloudinary();
+
+// middleware
+
+app.use(express.json());
+app.use(cors())
+
+// api endpoints:
+
+app.get('/', (req, res) => {
+    res.send("API WORKING")
+});
+
+// start server
+app.listen(port, () => { 
+    console.log("Server is running on port:",port)
+})
