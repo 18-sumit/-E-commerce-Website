b50a05d488f1c3ee8a404e3a333aeed756d95557
Sumit Singh <sumitsingh3076@gmail.com>
2024-12-12 23:43:26 +0530

    config dotenv

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,11 @@ import 'dotenv/config'
 import connectDB from "./config/mongodb.js";
 import uploadOnCloudinary from "./config/cloudinary.js";
 import userRouter from "./routes/user.routes.js";
+import dotenv from "dotenv";
 
+dotenv.config({
+    path: "./env"
+})
 
 //App config
 
@@ -20,13 +24,13 @@ app.use(express.json());
 app.use(cors())
 
 // api endpoints:
-app.use('/api/user',userRouter)
+app.use('/api/user', userRouter)
 
 app.get('/', (req, res) => {
     res.send("API WORKING")
 });
 
 // start server
-app.listen(port, () => { 
-    console.log("Server is running on port:",port)
+app.listen(port, () => {
+    console.log("Server is running on port:", port)
 })
