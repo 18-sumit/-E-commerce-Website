0a5a36c95b407b64cca9a840e22dcf760add8597
Sumit Singh <sumitsingh3076@gmail.com>
2024-12-16 00:13:36 +0530

    initial config

---
+++ Admin/src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import AdminNavbar from './components/AdminNavbar'
+
+const App = () => {
+  return (
+    <>
+      <AdminNavbar />
+
+    </>
+  )
+}
+
+export default App
