caa1e3313e23d8bbc97003d5a0594493f027186c
Sumit Singh <sumitsingh3076@gmail.com>
2024-12-17 00:21:10 +0530

    minor twerks

--- Admin/tailwind.config.js
+++ Admin/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './index.html',          // Include your static HTML file(s)
+    './src/**/*.{js,ts,jsx,tsx}',  // Include JS/TS/JSX/TSX files
   ],
   theme: {
     extend: {},
