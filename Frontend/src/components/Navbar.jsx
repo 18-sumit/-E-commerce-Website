fefbc4406c27a95951f43384a780cf6a64a601d3
Sumit Singh <sumitsingh3076@gmail.com>
2024-12-20 21:47:13 +0530

    feat: Improve Navbar with user profile dropdown and mobile menu
    - Added dropdown for user profile options (My Profile, Orders, Logout).
    - Implemented responsive side menu for mobile navigation.
    - Enhanced logout functionality to clear user session and cart items.

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -6,7 +6,14 @@ import { ShopContext } from "../context/ShopContext.jsx";
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
 
-  const { setShowSearch, getCartCount } = useContext(ShopContext);
+  const { setShowSearch, getCartCount, navigate, accessToken, setAccessToken, setCartItems } = useContext(ShopContext);
+
+  const logout = () => {
+    navigate('/login')
+    localStorage.removeItem('accessToken');
+    setAccessToken('');
+    setCartItems({});
+  }
 
   return (
     // Logo section
@@ -38,21 +45,25 @@ const Navbar = () => {
         <img onClick={() => setShowSearch(true)} src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
 
         <div className="group relative">
-          <Link to="/login">
-            <img
-              className="w-5 cursor-pointer"
-              src={assets.profile_icon}
-              alt=""
-            />
-          </Link>
 
-          <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
-            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
-              <p className="cursor-pointer hover:text-black">My Profile</p>
-              <p className="cursor-pointer hover:text-black">Orders</p>
-              <p className="cursor-pointer hover:text-black">Logout</p>
+          <img
+            onClick={() => accessToken ? null : navigate('login')}
+            className="w-5 cursor-pointer"
+            src={assets.profile_icon}
+            alt=""
+          />
+
+          {/* Dropdown Menu */}
+          {
+            accessToken &&
+            <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+              <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
+                <p className="cursor-pointer hover:text-black">My Profile</p>
+                <p onClick={() => navigate('/orders')} className="cursor-pointer hover:text-black">Orders</p>
+                <p onClick={logout} className="cursor-pointer hover:text-black">Logout</p>
+              </div>
             </div>
-          </div>
+          }
         </div>
         <Link to="/cart" className="relative">
           <img src={assets.cart_icon} className="w-5 min-w-5" alt="" />
