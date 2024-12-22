0260fc1d8ebc6d8b6207468ae23d353348450afa
Sumit Singh <sumitsingh3076@gmail.com>
2024-12-22 14:59:42 +0530

    Add order routes for user and admin .

---
+++ Backend/routes/order.route.js
@@ -0,0 +1,21 @@
+import { Router } from 'express'
+import { placeOrderCod, placeOrderRazorPay, placeOrderStripe, allOrders, userOrders, updateOrderStatus } from '../controllers/order.controller.js'
+import adminAuth from '../middleware/adminAuth.middleware.js'
+import authUser from '../middleware/authUser.middleware.js'
+
+const orderRouter = Router.express()
+
+// Admin Features
+orderRouter.post('/list', adminAuth, allOrders)
+orderRouter.post('/update-order-status', adminAuth, updateOrderStatus)
+
+
+//Payment Features
+orderRouter.post('/cod', authUser, placeOrderCod);
+orderRouter.post('/stripe', authUser, placeOrderStripe);
+orderRouter.post('/razorpay', authUser, placeOrderRazorPay);
+
+// User Features 
+orderRouter.post('/userorders', authUser, userOrders)
+
+export default orderRouter
