04c4b7c1d6714099dfdd49d08cce2b5117e0c5f4
Sumit Singh <sumitsingh3076@gmail.com>
2024-12-23 13:22:18 +0530

    orderRouter added

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,7 @@ import userRouter from "./routes/user.routes.js";
 import dotenv from "dotenv";
 import productRouter from "./routes/product.route.js";
 import cartRouter from "./routes/cart.route.js";
+import orderRouter from "./routes/order.route.js"
 
 dotenv.config({
     path: "./env"
@@ -30,7 +31,9 @@ app.use('/api/user', userRouter)
 app.use('/api/product', productRouter)
 
 //cart api endpoints:
-app.use('/api/cart' , cartRouter)
+app.use('/api/cart', cartRouter)
+//order api endpoints:
+app.use('/api/order', orderRouter)
 
 
 app.get('/', (req, res) => {
